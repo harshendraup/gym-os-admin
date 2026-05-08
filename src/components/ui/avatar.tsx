479724dc76951a09ed432802609d43e6ce68930c
prakashgupta27 <prakashgupta272001@gmail.com>
2026-05-08 12:01:45 +0530

    test commit

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import * as AvatarPrimitive from '@radix-ui/react-avatar'
 import { cn } from '@/lib/utils'
 
+console.log("Test")
+
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
